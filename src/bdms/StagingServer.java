cbc6ce97ce35e54b1074b5024c038fc7206e7664
saibabu-befree <31272036+saibabu-befree@users.noreply.github.com>
2018-04-03 13:32:04 +0530

    Update StagingServer.java

--- src/bdms/StagingServer.java
+++ src/bdms/StagingServer.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-public class StagingServe {
+public class StagingServer {
 	
 	@Test
 	public void testjenkins() {
